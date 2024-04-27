33028d9f37a0f08250d423da88cc42e08b418d11
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2024-04-27 10:18:45 +0700

    change index

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Test Github Action by Hoang && Tuan</a>
         </h1>
 
         <p className={styles.description}>
